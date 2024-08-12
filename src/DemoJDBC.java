2ff0864a823168bac0114cddfda0e4551a2f9a66
asmitgupta1 <guptaasmit1@gmail.com>
2024-08-12 22:12:10 +0530

    adding query

--- src/DemoJDBC.java
+++ src/DemoJDBC.java
@@ -8,10 +8,23 @@ public class DemoJDBC {
         String url ="jdbc:postgresql://localhost:5432/test";
         String uname ="postgres";
         String pass ="0000";
-
+        String query = "SELECT * FROM public.student";
 //        Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url,uname,pass);
 
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+//        rs.next();
+//        System.out.println(rs.next());
+//        System.out.println( rs.getString("sname"));
+
         System.out.println("connection Established");
+
+            while(rs.next())
+            {
+                System.out.println(rs.getInt("marks") + " "+ rs.getString("sname"));
+            }
+        con.close();
+        System.out.println("connection Closed");
     }
 }
